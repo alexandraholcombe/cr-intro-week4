c083e01983894f4913fb9e0a36b9931b695708be
Alexandra Holcombe <alexandra.holcombe@gmail.com>
2017-01-27 14:13:06 -0800

    Create Array to add text names of toppings to Pizza object

--- js/scripts.js
+++ js/scripts.js
@@ -34,34 +34,37 @@ var pineapple = new Topping ('pineapple', 'Pineapple', 1);
 
 var toppings = [pepperoni, sausage, bacon, canadianBacon, bananaPeppers, blackOlives, greenPeppers, mushrooms, onions, spinach, pineapple];
 
-//CREATE NEW Pizza
-function newPizza (size, toppings) {
+//CREATE NEW Pizza Constructor
+function Pizza (size) {
   this.size = size;
   this.toppings = [];
 }
+var newPizza;
 
 //User Interface Logic
 $(function() {
   //Add Size objects to page
   $.each(sizes, function(i, val){
-    // debugger;
     $("#pizza-size").append("<div class='radio'>" +
                               "<label>" +
-                              "<input type='radio' name='pizza_sizes' value='" + sizes[i] + "'> " +
+                              "<input type='radio' name='pizza_sizes' data-size='" + sizes[i] + "'> " +
                               sizes[i].displayName + "</label></div>");
   })
 
   //Add Toppings objects to page
   $.each(toppings, function(i, val){
+    // debugger;
     $("#pizza-toppings").append("<div class='checkbox'>" +
                               "<label>" +
-                              "<input type='checkbox' name='pizza_toppings' value='" + toppings[i] + "'>" +
-                              toppings[i].displayName + "</label></div>").data(this);
+                              "<input type='checkbox' name='pizza_toppings' data-topping='" + toppings[i] + "'>" +
+                              toppings[i].displayName + "</label></div>");
   })
 
   //Select size and apply to order
   $("#pizza-size div label input").click(function () {
     $("#chosen-size").text(this.parentElement.textContent);
+    newPizza = new Pizza (this.parentElement.textContent);
+    // debugger;
   })
 
   //Select topping and apply to order
@@ -69,8 +72,8 @@ $(function() {
     // debugger;
     if (this.checked) {
       $("<li>").text(this.parentElement.textContent).appendTo("#chosen-toppings").off();
+      newPizza.toppings.push(this.parentElement.textContent);
       debugger;
-      console.log(this.data);
     } else {
       //Remove topping from order
       $("#chosen-toppings li:contains('" + this.parentElement.textContent + "')").remove();
