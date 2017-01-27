82214ec89bbf46299cc0947190c26e80f0fd4602
Alexandra Holcombe <alexandra.holcombe@gmail.com>
2017-01-27 10:45:49 -0800

    Append toppings objects into form

--- js/scripts.js
+++ js/scripts.js
@@ -12,8 +12,6 @@ var mediumPizza = new Size ('medium', 'Medium (12")', 12);
 var largePizza = new Size ('large', 'Large (14")', 14);
 
 var sizes = [smallPizza, mediumPizza, largePizza];
-var toppings = [];
-
 //CREATING TOPPINGS
 function Topping (value, displayName, price) {
   this.value = value,
@@ -21,17 +19,19 @@ function Topping (value, displayName, price) {
   this.price = price
 }
 
-var pepperoni = new Topping ('pepperoni', 'Pepperoni', 1)
-var sausage = new Topping ('sausage', 'Sausage', 1)
-var bacon = new Topping ('bacon', 'Bacon', 1)
-var canadianBacon = new Topping ('canadianBacon', 'Canadian Bacon', 1)
-var bananaPeppers = new Topping ('bananaPeppers', 'Banana Peppers', 1)
-var blackOlives = new Topping ('blackOlives', 'Black Olives', 1)
-var greenPeppers = new Topping ('greenPeppers', 'Green Peppers', 1)
-var mushrooms = new Topping ('mushrooms', 'Mushrooms', 1)
-var onions = new Topping ('onions', 'Onions', 1)
-var spinach = new Topping ('spinach', 'Spinach', 1)
-var pineapple = new Topping ('pineapple', 'Pineapple', 1)
+var pepperoni = new Topping ('pepperoni', 'Pepperoni', 1);
+var sausage = new Topping ('sausage', 'Sausage', 1);
+var bacon = new Topping ('bacon', 'Bacon', 1);
+var canadianBacon = new Topping ('canadianBacon', 'Canadian Bacon', 1);
+var bananaPeppers = new Topping ('bananaPeppers', 'Banana Peppers', 1);
+var blackOlives = new Topping ('blackOlives', 'Black Olives', 1);
+var greenPeppers = new Topping ('greenPeppers', 'Green Peppers', 1);
+var mushrooms = new Topping ('mushrooms', 'Mushrooms', 1);
+var onions = new Topping ('onions', 'Onions', 1);
+var spinach = new Topping ('spinach', 'Spinach', 1);
+var pineapple = new Topping ('pineapple', 'Pineapple', 1);
+
+var toppings = [pepperoni, sausage, bacon, canadianBacon, bananaPeppers, blackOlives, greenPeppers, mushrooms, onions, spinach, pineapple];
 
 //User Interface Logic
 $(function() {
@@ -43,9 +43,20 @@ $(function() {
                               sizes[i].displayName + "</label></div>");
   })
 
+  //Add Toppings objects to page
+  $.each(toppings, function(i, val){
+    $("#pizza-toppings").append("<div class='checkbox'>" +
+                              "<label>" +
+                              "<input type='checkbox' name='pizza_toppings' value='" + toppings[i].value + "'> " +
+                              toppings[i].displayName + "</label></div>");
+  })
+
   //Select size and apply to order
   $("#pizza-size div label input").click(function () {
     $("#chosen-size").text(this.parentElement.textContent);
   })
+
+  //Select topping and apply to order
+
 
 })
